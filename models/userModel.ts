0ada006ef05fcc1cea9d9504a86852882f4ace10
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2024-07-02 21:29:19 -0300

    criado função para buscar usuários

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,7 +1,24 @@
-export interface UserModel {
+import { client } from "../services/apiConfig.ts";
+
+interface UserModel {
   id: number;
   username: string;
   password: string;
   createdAt: Date;
   updatedAt: Date;
 }
+
+const findByUsername = async (username: string) => {
+  try {
+    await client.connect();
+    const result = await client.queryObject<UserModel>(
+      `SELECT * FROM users WHERE username = $username`,
+      { username: username }
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("error", error);
+  }
+};
+
+export { type UserModel, findByUsername };
