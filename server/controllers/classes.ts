acbcaf66a3c74802f6e927335a4b228ad2d7df08
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2024-07-08 20:59:00 -0300

    adicionado sub rotas para /classes

--- server/controllers/classes.ts
+++ server/controllers/classes.ts
@@ -1,12 +1,76 @@
-import { Context } from "https://deno.land/x/oak@v16.1.0/mod.ts";
+import { Context, Status } from "https://deno.land/x/oak@v16.1.0/mod.ts";
 import { client } from "../services/apiConfig.ts";
 
 const indexClasses = async (context: Context) => {
+  const { headers } = context.request;
+
   try {
-    console.log("context", context);
+    await client.connect();
+
+    const result = await client.queryObject(
+      `
+      SELECT CAST(c.id AS VARCHAR), D.abbreviation || ' - ' || D."name" AS description
+      FROM disciplines d
+      LEFT JOIN classes c ON c.discipline_id = d.id
+      WHERE c.teacher_id = $teacher_id
+      `,
+      { teacher_id: 3 }
+    );
+
+    if (result.rowCount) {
+      context.response.status = Status.OK;
+      context.response.body = result.rows;
+    }
   } catch (error) {
-    console.error("error", error);
+    context.response.status = Status.InternalServerError;
+    context.response.body = error;
   }
 };
 
-export { indexClasses };
+const indexPlanning = async (context: Context) => {
+  try {
+    await client.connect();
+
+    const result = await client.queryObject(
+      `
+      SELECT CAST(id AS VARCHAR), CAST(class_id AS VARCHAR), title, description, planned_date, applied_date
+      FROM class_planning  
+      WHERE class_id = $class_id
+      `,
+      { class_id: 2 }
+    );
+
+    if (result.rowCount) {
+      context.response.status = Status.OK;
+      context.response.body = result.rows;
+    }
+  } catch (error) {
+    context.response.status = Status.InternalServerError;
+    context.response.body = error;
+  }
+};
+
+const indexMaterials = async (context: Context) => {
+  try {
+    await client.connect();
+
+    const result = await client.queryObject(
+      `
+      SELECT CAST(id AS VARCHAR), CAST(class_id AS VARCHAR), title, description, file_format, file_url, list_index
+      FROM public.class_materials 
+      WHERE class_id = $class_id
+      `,
+      { class_id: 2 }
+    );
+
+    if (result.rowCount) {
+      context.response.status = Status.OK;
+      context.response.body = result.rows;
+    }
+  } catch (error) {
+    context.response.status = Status.InternalServerError;
+    context.response.body = error;
+  }
+};
+
+export { indexClasses, indexPlanning, indexMaterials };
--- server/routes/classes.ts
+++ server/routes/classes.ts
@@ -1,9 +1,16 @@
 import { Router } from "https://deno.land/x/oak@v16.1.0/mod.ts";
-import { indexClasses } from "../controllers/classes.ts";
-import { authMiddleware } from "../middlewares/authMiddleware.ts";
+import {
+  indexClasses,
+  indexPlanning,
+  indexMaterials,
+} from "../controllers/classes.ts";
 
 const classesRouter = new Router();
 
-classesRouter.get("/", authMiddleware, indexClasses);
+// route: /classes
+classesRouter
+  .get("/", indexClasses)
+  .get("/planning", indexPlanning)
+  .get("/materials", indexMaterials);
 
 export { classesRouter };
