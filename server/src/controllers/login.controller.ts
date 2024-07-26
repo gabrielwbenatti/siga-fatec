5ed7bdbc29e7ff1e30f0d70faf65ea1f06f6ae13
Gabriel Benatti <gabriel@infonacci.com.br>
2024-07-26 13:24:48 -0300

    migrado LOGIN route

---
+++ server/src/controllers/login.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import client from "../services/db";
+import User from "../models/user.model";
+import { compare } from "bcrypt";
+import { StatusCode } from "../utils/http.status.code";
+
+const login = async (req: Request, res: Response) => {
+  const body = req.body;
+
+  await client
+    .authenticate()
+    .then(async () => {
+      const user = await User.findOne({ where: { username: body.username } });
+
+      if (!user) throw new Error("User not found");
+
+      const isMatch = await compare(body.password, user.password);
+
+      if (!isMatch) throw new Error("Invalid Credentials");
+
+      const payload = { id: user.id };
+      res.send({ data: payload });
+    })
+    .catch((error) => {
+      res.statusCode = StatusCode.Unauthorized;
+      res.send({ message: error.message });
+    })
+    .finally(() => {
+      client.close();
+      res.end();
+    });
+};
+
+export { login };
--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import userRouter from "./users.router";
 import teacherRouter from "./teacher.router";
 import classRouter from "./classes.router";
+import loginRouter from "./login.router";
 
 const router = Router();
 
 router
   .use("/api/v1", userRouter)
   .use("/api/v1", teacherRouter)
+  .use("/api/v1/login", loginRouter)
   .use("/api/v1/classes", classRouter);
 
 export default router;
