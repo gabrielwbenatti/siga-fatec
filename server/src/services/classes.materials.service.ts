eb48b0b4628f53b9e4c29d7e99495a994e035468
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2025-02-20 21:59:52 -0300

    tratativa para body array ou nao

--- server/src/services/classes.materials.service.ts
+++ server/src/services/classes.materials.service.ts
@@ -4,13 +4,16 @@ class ClassesMaterialsService {
   getClassesMaterials = async (classId: number) => {
     const result = await db.class_materials.findMany({
       where: { class_id: classId },
+      orderBy: { list_index: "asc" },
     });
 
     return result;
   };
 
   createClassesMaterials = async (body: any) => {
-    const result = await db.class_materials.createMany(body);
+    const result = Array.isArray(body)
+      ? await db.class_materials.createMany({ data: body })
+      : await db.class_materials.create({ data: body });
 
     return result;
   };
