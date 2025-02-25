c9b8f25bddd3bf9f041e72101057bab098b15bc8
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2025-02-25 20:45:21 -0300

    removido temporariamente DND

--- siga-web/app/(application)/home/materials/page.tsx
+++ siga-web/app/(application)/home/materials/page.tsx
@@ -6,63 +6,7 @@ import { ROUTES } from "@/config/routes";
 import ClassMaterial from "@/types/ClassMaterial";
 import { ArrowDownUp, DownloadCloudIcon, Trash2 } from "lucide-react";
 import { FC, ReactNode, Ref, useEffect, useState } from "react";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { toast } from "sonner";
-
-interface DraggableItemProps {
-  item: ClassMaterial;
-  moveItem: (dragIndex: number, hoverIndex: number) => void;
-}
-const DraggableItem: FC<DraggableItemProps> = ({
-  item,
-  moveItem,
-}: DraggableItemProps) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: "ITEM",
-    item: { ...item },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  }));
-
-  return (
-    <div
-      ref={drag as unknown as Ref<HTMLDivElement>}
-      className={`flex select-none items-center rounded-lg p-2 ${isDragging ? "opacity-50" : "opacity-100"}`}
-      key={item.id}
-    >
-      <span className="p-2 text-gray-400 hover:cursor-pointer">
-        <ArrowDownUp className="size-5" />
-      </span>
-      <div>{`(${item.list_index}) - ${item.title}`}</div>
-    </div>
-  );
-};
-
-interface DraggableAreaProps {
-  children: ReactNode;
-  onDrop: (item: ClassMaterial) => void;
-}
-const DraggableArea: FC<DraggableAreaProps> = ({
-  children,
-  onDrop,
-}: DraggableAreaProps) => {
-  const [{ isOver }, drop] = useDrop({
-    accept: "ITEM",
-    drop: (item: ClassMaterial) => onDrop(item),
-    collect: (monitor) => ({ isOver: monitor.isOver() }),
-  });
-
-  return (
-    <div
-      ref={drop as unknown as Ref<HTMLDivElement>}
-      className={`${isOver ? "bg-red-50" : "bg-blue-50"}`}
-    >
-      {children}
-    </div>
-  );
-};
 
 const HomeMaterialsPage = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -120,43 +64,40 @@ const HomeMaterialsPage = () => {
   }
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="space-y-4 p-4">
-        <div className="flex justify-between">
-          <h1 className="text-2xl font-bold">Materiais de Aula</h1>
-          <div className="flex gap-1.5">
-            <Button
-              variant="secondary"
-              onClick={() => {
-                toast("Em breve");
-                // setReordering(!reordering);
-              }}
-            >
-              <ArrowDownUp /> Reordenar
-            </Button>
+    <div className="space-y-4 p-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Materiais de Aula</h1>
+        <div className="flex gap-1.5">
+          <Button
+            variant="secondary"
+            onClick={() => {
+              toast("Em breve");
+              // setReordering(!reordering);
+            }}
+          >
+            <ArrowDownUp /> Reordenar
+          </Button>
 
-            <a href={ROUTES.MATERIALS.CREATE}>
-              <Button>Novo</Button>
-            </a>
-          </div>
+          <a href={ROUTES.MATERIALS.CREATE}>
+            <Button>Novo</Button>
+          </a>
         </div>
+      </div>
 
-        {data?.length === 0 ? (
-          <div>Nenhuma informação para ser exibida.</div>
-        ) : (
-          <div className="flex flex-col divide-y">
-            {reordering ? (
-              <DraggableArea onDrop={() => {}}>
-                {data?.map((e, i) => (
-                  <DraggableItem
-                    item={{ ...e, list_index: i }}
-                    moveItem={moveItem}
-                    key={i}
-                  />
-                ))}
-              </DraggableArea>
-            ) : (
-              data?.map((e, i) => (
+      {data?.length === 0 ? (
+        <div>Nenhuma informação para ser exibida.</div>
+      ) : (
+        <div className="flex flex-col divide-y">
+          {reordering
+            ? data?.map((e, i) => (
+                <div
+                  className="flex select-none items-center rounded-lg p-2"
+                  key={i}
+                >
+                  {`(${e.list_index}) - ${e.title}`}
+                </div>
+              ))
+            : data?.map((e, i) => (
                 <div
                   className="flex items-center justify-between rounded-lg p-2 hover:bg-primary/10"
                   key={i}
@@ -186,12 +127,10 @@ const HomeMaterialsPage = () => {
                     </Button>
                   </div>
                 </div>
-              ))
-            )}
-          </div>
-        )}
-      </div>
-    </DndProvider>
+              ))}
+        </div>
+      )}
+    </div>
   );
 };
 
