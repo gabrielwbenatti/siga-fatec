6013d15e7335fef65d22e443be722d03177c656e
Infonacci Gabriel <gabriel@infonacci.com.br>
2025-03-05 15:20:07 -0300

    add geração de token jwt

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,8 +1,13 @@
+import { compare } from "bcrypt";
 import db from "../config/database";
-import * as bcrypt from "bcrypt";
+import { SignJWT } from "jose";
 
 class AuthService {
   signIn = async (body: any) => {
+    const secretKey = process.env.SECRET_KEY;
+    const secret = new TextEncoder().encode(secretKey);
+    const alg = "HS256";
+
     const user = await db.users.findFirst({
       include: { teacher: true },
       where: {
@@ -14,14 +19,26 @@ class AuthService {
       throw new Error("User not found");
     }
 
-    const isMatch = await bcrypt.compare(body.password, user.password);
+    const isMatch = await compare(body.password, user.password);
 
     if (!isMatch) {
       throw new Error("Invalid Credentials");
     }
 
     const [teacher] = user.teacher;
-    return { username: user.username, id: user.id, teacher_id: teacher.id };
+
+    const jwt = await new SignJWT({
+      user_id: user.id,
+      teacher_id: teacher.id,
+    })
+      .setProtectedHeader({ alg })
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .sign(secret);
+
+    return {
+      token: jwt,
+    };
   };
 }
 
