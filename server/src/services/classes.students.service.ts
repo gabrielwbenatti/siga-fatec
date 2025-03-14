ad6566b444e87bc308b74370f10492dc354bf0b9
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2025-03-14 11:33:45 -0300

    criado rotas de visualização de alunos

--- server/src/services/classes.students.service.ts
+++ server/src/services/classes.students.service.ts
@@ -2,12 +2,12 @@ import db from "../config/database";
 
 class ClassesStudentsServices {
   getClassesStudents = async (classId: number) => {
-    const result = await db.class_students.findMany({
-      include: { student: true },
-      where: { class_id: classId },
+    const students = await db.students.findMany({
+      include: { user: { select: { email: true } } },
+      where: { class_students: { some: { class_id: classId } } },
     });
 
-    return result;
+    return students;
   };
 }
 
