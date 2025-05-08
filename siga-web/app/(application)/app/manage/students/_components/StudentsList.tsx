d256071d0ec22f3327d0d2172b2046fe02bc4a46
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2025-05-08 20:38:46 -0300

    🔥 removido códigos antigos

--- siga-web/app/(application)/app/manage/students/_components/StudentsList.tsx
+++ siga-web/app/(application)/app/manage/students/_components/StudentsList.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { fetchStudents } from "@/app/actions/studentsAction";
-import { Collapsable } from "@/components/SiGA/Collapsable";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -12,15 +10,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { IStudentResponse } from "@/types/Student";
-import { Mail } from "lucide-react";
 import { FC, useEffect, useState } from "react";
 import { toast } from "sonner";
 
 const StudentsList: FC = () => {
   const [data, setData] = useState<IStudentResponse[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  const [expandedItems, setExpandedItems] = useState<number[]>([]); // State to track open/closed items
 
   useEffect(() => {
     async function fetchData() {
@@ -37,13 +32,6 @@ const StudentsList: FC = () => {
 
     fetchData();
   }, []);
-
-  // Toggle function
-  const toggleItem = (id: number) => {
-    setExpandedItems((prev) =>
-      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id],
-    );
-  };
 
   if (isLoading) {
     return <div>Carregando...</div>;
@@ -75,56 +63,6 @@ const StudentsList: FC = () => {
             ))}
           </TableBody>
         </Table>
-      </div>
-
-      <div className="flex flex-col gap-3">
-        {data.map((s) => (
-          <Collapsable.Root key={s.id}>
-            <Collapsable.Header
-              onClick={() => toggleItem(s.id)}
-              actions={
-                <a href={`mailto:${s.user.email}`}>
-                  <Button size="icon" variant="outline">
-                    <Mail />
-                  </Button>
-                </a>
-              }
-            >
-              <span>{`${s.first_name} ${s.last_name}`}</span>
-            </Collapsable.Header>
-
-            {expandedItems.includes(s.id) && (
-              <Collapsable.Body>
-                <div className="flex flex-col md:grid md:grid-cols-3">
-                  <div className="flex flex-col">
-                    <span className="text-sm font-bold text-gray-500">
-                      E-mail
-                    </span>
-                    <span className="overflow-ellipsis">{s.user.email}</span>
-                  </div>
-
-                  <div className="flex flex-col">
-                    <span className="text-sm font-bold text-gray-500">
-                      Telefone 1
-                    </span>
-                    <div className="overflow-ellipsis">
-                      {s.phone1 ? s.phone1 : "--"}
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col">
-                    <span className="text-sm font-bold text-gray-500">
-                      Telefone 2
-                    </span>
-                    <div className="overflow-ellipsis">
-                      {s.phone2 ? s.phone2 : "--"}
-                    </div>
-                  </div>
-                </div>
-              </Collapsable.Body>
-            )}
-          </Collapsable.Root>
-        ))}
       </div>
     </>
   );
